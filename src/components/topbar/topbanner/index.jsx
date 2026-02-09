03aefe425da0354b7d3cdb1096f38653f84ee87a
fsadao-fsa <fsadao@gmail.com>
2026-02-09 18:58:59 -0300

    Adição do logo responsivo

--- src/components/topbar/topbanner/index.jsx
+++ src/components/topbar/topbanner/index.jsx
@@ -1,8 +1,10 @@
+import DoutorWakaLogo from "./images/logo.png";
 import "./style.css";
 
 export function Topbanner() {
     return (
         <div className="topbanner">
+            <img className="topbanner" src={DoutorWakaLogo} alt="Dr. Waka" />
             <center className="topbanner">GERENCIADOR DE FILMES</center>
         </div>
     );
