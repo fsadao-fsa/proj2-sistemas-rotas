469c2f54081f97be044b928f2ebf7a2b8dd73049
fsadao-fsa <fsadao@gmail.com>
2026-02-10 17:00:46 -0300

    Configuracao do menu/link

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 import "./style.css";
 
 export function Menu() {
     return (
-        <div className="menu">HOME</div>
+        <div className="menu">
+            <nav className="menu">
+                <Link className="menu" to="/">HOME</Link>
+            </nav>
+            <nav className="menu">
+                <Link className="menu" to="/">FILMES</Link>
+            </nav>
+            <nav className="menu">
+                <Link className="menu" to="/">DIRETORES</Link>
+            </nav>
+            <nav className="menu">
+                <Link className="menu" to="/">ATORES</Link>
+            </nav>
+        </div>
     );
 }
