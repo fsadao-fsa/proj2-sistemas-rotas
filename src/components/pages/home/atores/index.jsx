0f0712ef9d9c881298304e685789defaed77d5b2
fsadao-fsa <fsadao@gmail.com>
2026-02-10 19:22:25 -0300

    Configuracao da variavel de ambiente

--- src/components/pages/home/atores/index.jsx
+++ src/components/pages/home/atores/index.jsx
@@ -1,6 +1,8 @@
 // import "./style.css"
+import { BACKEND_URL } from "../../../../config";
 
 export function Atores() {
+    console.log("Backend: " + BACKEND_URL);
     return (
         <div className="content">
             <h1 >ATORES</h1>
