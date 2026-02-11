b4e3022127f803c1cada9b54d4d050cd26805103
fsadao-fsa <fsadao@gmail.com>
2026-02-11 16:47:57 -0300

    Utilizando useState

--- src/components/pages/home/atores/index.jsx
+++ src/components/pages/home/atores/index.jsx
@@ -1,12 +1,41 @@
-// import "./style.css"
+import "./style.css";
 import { BACKEND_URL } from "../../../../config";
+import axios from "axios";
+import { useState } from "react";
+
+function atoresHandler(atores) {
+    if (!Array.isArray(atores)) {
+        return (atores)
+    }
+    var i = 0;
+    var content = atores.map(function (atores) {
+        return (
+            <div key={i++}>{atores.nome}</div>
+        )
+    });
+
+    return content;
+}
 
 export function Atores() {
-    console.log("Backend: " + BACKEND_URL);
+    const [atores, setAtores] = useState("Nada a mostrar");
+
+    axios.get(BACKEND_URL + "/atores")
+        .then(function (response) {
+            setAtores(atoresHandler(response.data));
+        })
+        .catch(function (error) {
+
+        })
+        .finally(function () {
+
+        })
+
     return (
         <div className="content">
             <h1 >ATORES</h1>
-            <div>CONTEÚDO</div>
+            <div>{atores}</div>
+            <p><button onClick={() => setAtores("Cliquei no botão")} >Clique aqui!</button></p>
         </div>
     );
 }
