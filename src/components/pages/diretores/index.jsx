adc701b7536b0e3eee4ae1ebe8d28a6ab469a221
fsadao-fsa <fsadao@gmail.com>
2026-02-27 19:43:31 -0300

    Trazendo os filmes e os diretores

--- src/components/pages/diretores/index.jsx
+++ src/components/pages/diretores/index.jsx
@@ -1,10 +1,57 @@
 import "./style.css"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { BACKEND_URL } from "../../../config";
+
+function diretoresHandler(diretores) {
+    if (!Array.isArray(diretores)) {
+        return (diretores)
+    }
+    var i = 0;
+    /*o map funciona com uma função e vai receber cada componente do meu vetor
+    atores vai ser uma linha do meu vetor iniciando pelo componente 0, 1 ...
+    O map irá rodar para cada linha do meu vetor*/
+    var content = diretores.map(function (diretores) {
+        return (
+            <div className="diretores-content" key={i++}>{i + 1} - {diretores.nome}</div>
+        )
+    });
+
+    return content;
+}
+
 
 export function Diretores() {
+
+    const [diretores, setDiretores] = useState("Nada a mostrar");
+
+    /*useEffect é um cara que vai rodar esta função em sua declação toda vez
+    que as variaveis do array mudarem. Por isso que não roda infnito. Executará
+    apenas na primeira vez que for montada*/
+    useEffect(function () {
+
+        /*Fazendo uma chamada http, no endereço atores, caso a chamada tenha
+        sido feito com sucesso entramos no response.data, que e o resposta, caso
+        tenha dado errado vai para o erro e termina com finally (sempre será 
+        executado. Assim que uma chamada axios funciona)
+         */
+        axios.get(BACKEND_URL + "/diretores")
+            .then(function (response) {
+                setDiretores(diretoresHandler(response.data));
+            })
+            .catch(function (error) {
+                console.log("Error:" + error);
+            })
+            .finally(function () {
+                console.log("Finally.");
+            })
+
+    }, []);
+
     return (
         <div className="content">
             <h1 >DIRETORES</h1>
-            <div>CONTEÚDO</div>
+            <div>{diretores}</div>
         </div>
     );
 }
--- src/components/pages/home/filmes/index.jsx
+++ src/components/pages/home/filmes/index.jsx
@@ -1,10 +1,57 @@
 import "./style.css"
 
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { BACKEND_URL } from "../../../../config";
+
+function filmesHandler(filmes) {
+    if (!Array.isArray(filmes)) {
+        return (filmes)
+    }
+    var i = 0;
+    /*o map funciona com uma função e vai receber cada componente do meu vetor
+    atores vai ser uma linha do meu vetor iniciando pelo componente 0, 1 ...
+    O map irá rodar para cada linha do meu vetor*/
+    var content = filmes.map(function (filmes) {
+        return (
+            <div className="filmes-content" key={i++}>{i + 1} - {filmes.nome}</div>
+        )
+    });
+
+    return content;
+}
+
 export function Filmes() {
+
+    const [filmes, setFilmes] = useState("Nada a mostrar");
+
+    /*useEffect é um cara que vai rodar esta função em sua declação toda vez
+    que as variaveis do array mudarem. Por isso que não roda infnito. Executará
+    apenas na primeira vez que for montada*/
+    useEffect(function () {
+
+        /*Fazendo uma chamada http, no endereço atores, caso a chamada tenha
+        sido feito com sucesso entramos no response.data, que e o resposta, caso
+        tenha dado errado vai para o erro e termina com finally (sempre será 
+        executado. Assim que uma chamada axios funciona)
+         */
+        axios.get(BACKEND_URL + "/filmes")
+            .then(function (response) {
+                setFilmes(filmesHandler(response.data));
+            })
+            .catch(function (error) {
+                console.log("Error:" + error);
+            })
+            .finally(function () {
+                console.log("Finally.");
+            })
+
+    }, []);
+
     return (
-        <div className="content">
+        <div className="filmes-content">
             <h1 >FILMES</h1>
-            <div>CONTEÚDO</div>
+            <div>{filmes}</div>
         </div>
     );
 }
