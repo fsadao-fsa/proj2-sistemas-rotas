fd4c1de0bf4cea3133e832d99303121fd91f89ea
fsadao-fsa <fsadao@gmail.com>
2026-02-27 19:19:20 -0300

    Revisao do codigo de atores

--- src/components/pages/home/atores/index.jsx
+++ src/components/pages/home/atores/index.jsx
@@ -1,16 +1,19 @@
 import "./style.css";
 import { BACKEND_URL } from "../../../../config";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function atoresHandler(atores) {
     if (!Array.isArray(atores)) {
         return (atores)
     }
     var i = 0;
+    /*o map funciona com uma função e vai receber cada componente do meu vetor
+    atores vai ser uma linha do meu vetor iniciando pelo componente 0, 1 ...
+    O map irá rodar para cada linha do meu vetor*/
     var content = atores.map(function (atores) {
         return (
-            <div key={i++}>{atores.nome}</div>
+            <div className="atores-content" key={i++}>{i + 1} - {atores.nome}</div>
         )
     });
 
@@ -20,16 +23,29 @@ function atoresHandler(atores) {
 export function Atores() {
     const [atores, setAtores] = useState("Nada a mostrar");
 
-    axios.get(BACKEND_URL + "/atores")
-        .then(function (response) {
-            setAtores(atoresHandler(response.data));
-        })
-        .catch(function (error) {
+    /*useEffect é um cara que vai rodar esta função em sua declação toda vez
+    que as variaveis do array mudarem. Por isso que não roda infnito. Executará
+    apenas na primeira vez que for montada*/
+    useEffect(function () {
 
-        })
-        .finally(function () {
+        /*Fazendo uma chamada http, no endereço atores, caso a chamada tenha
+        sido feito com sucesso entramos no response.data, que e o resposta, caso
+        tenha dado errado vai para o erro e termina com finally (sempre será 
+        executado. Assim que uma chamada axios funciona)
+         */
+        axios.get(BACKEND_URL + "/atores")
+            .then(function (response) {
+                setAtores(atoresHandler(response.data));
+            })
+            .catch(function (error) {
+                console.log("Error:" + error);
+            })
+            .finally(function () {
+                console.log("Finally.");
+            })
 
-        })
+    }, []);
+
 
     return (
         <div className="content">
